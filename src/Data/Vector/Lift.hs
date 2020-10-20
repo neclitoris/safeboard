0d77c885ae6d1a4857eb68e84b02c9f21d2347d5
Nikita Solodovnikov <nas140301@gmail.com>
2020-10-20 17:03:10 +0300

    optimize vector lift instance

--- src/Data/Vector/Lift.hs
+++ src/Data/Vector/Lift.hs
@@ -1,9 +1,22 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Data.Vector.Lift where
 
-import Data.Vector.Storable
+import qualified Data.Vector.Storable as V
+import Data.Word
+import Foreign.Storable
+import GHC.ForeignPtr
+import GHC.Ptr
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
+import System.IO.Unsafe
 
-instance (Storable a, Lift a) => Lift (Vector a) where
-    lift = appE (varE 'fromList) . lift . toList
+instance (Storable a) => Lift (V.Vector a) where
+  lift v = [|
+    unsafePerformIO $ do
+      ptr <- newForeignPtr_ (Ptr $(bytes))
+      return $ V.unsafeFromForeignPtr ptr offset length
+    |]
+    where
+      bytes = litE $ StringPrimL $ V.toList $ (V.unsafeCast v :: V.Vector Word8)
+      offset = 0 :: Int
+      length = V.length v
